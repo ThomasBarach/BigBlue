aa9037513e1c37bafc0dfbd2456a38b4af0b8934
Thomas Barach <tbarach@intechinfo.fr>
2017-01-26 16:30:52 +0100

    Update host.sh
    Bash script to feed Shinken

---
+++ scripts/host.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Bash script to feed shinken host files
+
+green = '\033[1;92m'
+red = '\033[1;91m'
+blue = '\033[1;94m'
+nc = '\033[0m'
+
+host = $1
+address = $2
+
+if [ -e /etc/shinken/hosts/$host.cfg ]; then
+  echo -e "${red}File $host already exists, let's move on${nc}"
+ 
+else 
+  touch /etc/shinken/hosts/$host.cfg
+  echo "define host {
+use         BigBlue
+host_name   $host
+address     $address
+hostgroups  BigBlues
+}" >> /etc/shinken/hosts/$host.cfg
+  
+  echo -e "${blue}File $host.cfg created${nc}"
+
+fi 
